a9cbf7f2d5e921aeef567e4a3ed815ddf3d28248
Rudy Suharyadi <rudy.suharyadi@gmail.com>
2024-12-09 17:44:07 +0700

    Init project with firebase

--- ebuddy/ebuddyApp.swift
+++ ebuddy/ebuddyApp.swift
@@ -7,9 +7,23 @@
 
 import SwiftUI
 import SwiftData
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
 
 @main
 struct ebuddyApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self,
