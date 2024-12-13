6597ea33d0e812218d52dc0e457702cee42353f0
Rudy Suharyadi <rudy.suharyadi@gmail.com>
2024-12-13 10:49:25 +0700

    Firestore multiple query

--- ebuddy/Manager/FirestoreManager.swift
+++ ebuddy/Manager/FirestoreManager.swift
@@ -40,4 +40,30 @@ class FirestoreManager {
         }
     }
     
+    // Assume lastActiveTimestampt, rating, and price is available to query.
+    func getRecommendedUser(completion: @escaping (Result<[User], Error>) -> Void) {
+        var query: Query = db.collection("USERS")
+        
+        query = query.order(by: "lastActiveTimestamp", descending: true)
+        query = query.order(by: "rating", descending: true)
+        query = query.whereField("gender", isEqualTo: GenderEnum.female.rawValue)
+        query = query.order(by: "price", descending: false)
+            
+        query.getDocuments { snapshot, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            do {
+                let users = try snapshot?.documents.compactMap {
+                    try $0.data(as: User.self)
+                } ?? []
+                completion(.success(users))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+    
 }
